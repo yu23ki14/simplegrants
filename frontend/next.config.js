07c9c5b35546d1b220ce3962bd2b69754e5b76c9
tkgshn <takagishunsuke1129@gmail.com>
2024-04-17 13:46:26 +0900

    #15

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -28,6 +28,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "**twimg.com",
       },
+      {
+        protocol: "https",
+        hostname: "www.digdao.jp",
+      },
     ],
   },
 };
